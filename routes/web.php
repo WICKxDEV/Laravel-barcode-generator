db38474a4c30823d3f84513086983b746f4bf06c
WICKxDEV <160169165+WICKxDEV@users.noreply.github.com>
2024-12-06 11:28:05 +0530

    create route for generating a barcode-save

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('barcode', function () {
+Route::get('barcode-save', function () {
     $generatorPNG = new Picqer\Barcode\BarcodeGeneratorPNG();
     $image = $generatorPNG->getBarcode('000005263635', $generatorPNG::TYPE_CODE_128);
+
+    Storage::put('barcodes/demo.png', $image);
 
     return response($image)->header('Content-type','image/png');
 });
