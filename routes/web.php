e466510257f6b9300de64e2479dee8887f4efa89
WICKxDEV <160169165+WICKxDEV@users.noreply.github.com>
2024-12-06 11:28:55 +0530

    create route for generating a barcode-blade

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('barcode-save', function () {
-    $generatorPNG = new Picqer\Barcode\BarcodeGeneratorPNG();
-    $image = $generatorPNG->getBarcode('000005263635', $generatorPNG::TYPE_CODE_128);
+Route::get('barcode-blade', function () {
+    $generatorHTML = new Picqer\Barcode\BarcodeGeneratorHTML();
+    $barcode = $generatorHTML->getBarcode('0001245259636', $generatorHTML::TYPE_CODE_128);
 
-    Storage::put('barcodes/demo.png', $image);
-
-    return response($image)->header('Content-type','image/png');
+    return view('barcode', compact('barcode'));
 });
