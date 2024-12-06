9f1228fa741c17c59635ea49675ccce4b215e42a
WICKxDEV <160169165+WICKxDEV@users.noreply.github.com>
2024-12-06 11:12:00 +0530

    create route for generating a barcode

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('barcode', function () {
+    $generatorPNG = new Picqer\Barcode\BarcodeGeneratorPNG();
+    $image = $generatorPNG->getBarcode('000005263635', $generatorPNG::TYPE_CODE_128);
+
+    return response($image)->header('Content-type','image/png');
+});
